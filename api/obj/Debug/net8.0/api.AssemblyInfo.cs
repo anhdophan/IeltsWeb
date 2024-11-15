fc79c66b07f5883195dab1462a7943495f59b769
Phan Đô <mazuongzero@gmail.com>
2024-11-15 19:39:42 +0700

    tets

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c43206b262615669fe6eb9710aa9cbc2d2aa3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c7cfbfd4c5caf0816f72cde7699fdaee4249b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
